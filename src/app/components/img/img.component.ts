6af467e937e6f9faf5d223469c005888501933b6
pjyeah <pespinoza9494@utm.edu.ec>
2023-09-22 21:06:22 -0500

    Capitulo 06

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,18 +1,52 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss'],
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges {
   @Input() img: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
 
-  constructor() {}
+  constructor() {
+    // before render
+    // NO async -- once time
+    console.log('constructor', 'imgValue =>', this.img);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // before - during render
+    // async -- once time
+    console.log('ngOnInit', 'imgValue =>', this.img);
+  }
+
+  ngOnChanges(changes: any) {
+    // before render
+    // changes inputs -- times
+    console.log('ngOnChanges', 'imgValue =>', this.img);
+  }
+
+  ngAfterViewInit() {
+    // after render
+    // handler children
+    console.log('ngAfterViewInit');
+  }
+
+  ngOnDestroy() {
+    // delete
+    console.log('ngOnDestroy');
+  }
 
   imgError() {
     this.img = this.imageDefault;
