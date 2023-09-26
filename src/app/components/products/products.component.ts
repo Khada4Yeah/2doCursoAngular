6dd3ff585096089a3fc25c22397f833cb8831851
pjyeah <pespinoza9494@utm.edu.ec>
2023-09-25 21:37:23 -0500

    Capitulo 12

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -2,14 +2,16 @@ import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../../models/product.model';
 
+import { StoreService } from '../../services/store.service';
+
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-  myShoppingCart: Product[] = [];
   total = 0;
+  myShoppingCart: Product[] = [];
   products: Product[] = [
     {
       id: '1',
@@ -37,12 +39,14 @@ export class ProductsComponent implements OnInit {
     },
   ];
 
-  constructor() {}
+  constructor(private storeService: StoreService) {
+    this.myShoppingCart = this.storeService.getShoppingCart();
+  }
 
   ngOnInit(): void {}
 
   onAddToShoppingCart(product: Product): void {
-    this.myShoppingCart.push(product);
-    this.total = this.myShoppingCart.reduce((sum, item) => sum + item.price, 0);
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
   }
 }
